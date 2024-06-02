84528f88889f09636a62fe31d8dffdc6bd8c1912
Asp1kkk <a5p1k@yandex.ru>
2024-06-02 20:33:49 +0300

    add: app

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,35 @@
 package app
 
-import "net/http"
+import (
+	"GOncurrently-Calculator/internal/server"
+	"context"
+	"time"
+)
 
 type App struct {
-	server *http.Server
+	server *server.Server
+
+	additionDuration       time.Duration
+	substractionDuration   time.Duration
+	multiplicationDuration time.Duration
+	divisionDuration       time.Duration
+}
+
+func New(addition, substraction, multiplication, division time.Duration, CP int) *App {
+	return &App{
+		server: server.New(),
+
+		additionDuration:       addition,
+		substractionDuration:   substraction,
+		multiplicationDuration: multiplication,
+		divisionDuration:       division,
+	}
+}
+
+func (ap *App) Run() {
+	ap.server.Run()
+}
+
+func (ap *App) Stop(ctx context.Context) {
+	ap.server.Stop(ctx)
 }
